62cafe35ea471871589719e9846021b57086f331
Luke Hodkinson <furious.luke@gmail.com>
2016-04-28 18:04:31 +1000

    Added a handler to reduce lists of objects that need to be quickly accessed by IDs.

--- src/reducers.js
+++ src/reducers.js
@@ -4,17 +4,17 @@ import { union } from './sets'
 // Check for handlers that have overlapping keys.
 // TODO: Throw a more useful error.
 function checkOverlappingHandlers( handlers ) {
-    if( handlers.length ) {
-        let all = new Set( Object.keys( handlers[0] ) );
-        let count = all.size;
-        for( var ii = 1; ii < handlers.length; ++ii ) {
-            let cur = Object.keys( handlers[ii] );
-            all = union( all, cur );
-            count += cur.length;
-        }
-        if( all.length != count )
-            throw 'Overlapping handlers.';
+  if( handlers.length ) {
+    let all = new Set( Object.keys( handlers[0] ) );
+    let count = all.size;
+    for( var ii = 1; ii < handlers.length; ++ii ) {
+      let cur = Object.keys( handlers[ii] );
+      all = union( all, cur );
+      count += cur.length;
     }
+    if( all.length != count )
+      throw 'Overlapping handlers.';
+  }
 }
 
 // Take a set of handlers and an initial state and produce
@@ -22,168 +22,236 @@ function checkOverlappingHandlers( handlers ) {
 // reducer call, i.e. ( state, action ).
 export function createReducer( handlers, initialState = {} ) {
 
-    // Before checking for overlap in the handlers, extract any subreducers.
-    let subreducers;
-    if( Array.isArray( handlers ) ) {
-        let count = 0;
-        subreducers = {};
-        handlers.forEach( handler => {
-            let cur = handler.SUBREDUCERS;
-            if( cur ) {
-                count += Object.keys( cur ).length;
-                subreducers = { ...subreducers, ...cur };
-            }
-        });
-        if( Object.keys( subreducers ).length != count )
-            throw 'Overlapping subreducers.';
-        if( count == 0 )
-            subreducers = undefined;
+  // Before checking for overlap in the handlers, extract any subreducers.
+  let subreducers;
+  if( Array.isArray( handlers ) ) {
+    let count = 0;
+    subreducers = {};
+    handlers.forEach( handler => {
+      let cur = handler.SUBREDUCERS;
+      if( cur ) {
+        count += Object.keys( cur ).length;
+        subreducers = { ...subreducers, ...cur };
+      }
+    });
+    if( Object.keys( subreducers ).length != count )
+      throw 'Overlapping subreducers.';
+    if( count == 0 )
+      subreducers = undefined;
+  }
+  else
+    subreducers = handlers.SUBREDUCERS;
+
+  // Handlers can be an array, indicating we want to merge a bunch of
+  // handlers together.
+  let allHandlers;
+  if( Array.isArray( handlers ) ) {
+    checkOverlappingHandlers( handlers );
+    allHandlers = Object.assign.apply( {}, handlers );
+  }
+  else
+    allHandlers = handlers;
+
+  return function reducer( state = initialState, action ) {
+
+    // Check for a local action.
+    // ASSUMPTION: Actions may appear only once in any tree.
+    if( action && allHandlers.hasOwnProperty( action.type ) )
+      return allHandlers[action.type]( state, action );
+
+    // Check for subreducers.
+    else if( subreducers ) {
+      let subState = {};
+      Object.keys( subreducers ).forEach( key => {
+        subState[key] = subreducers[key]( state[key], action );
+      });
+      return Object.assign( {}, state, subState );
     }
+
+    // Check for a default action.
+    else if( allHandlers.hasOwnProperty( 'DEFAULT' ) )
+      return allHandlers.DEFAULT( state, action );
+
     else
-        subreducers = handlers.SUBREDUCERS;
+      return state;
+  }
+}
 
-    // Handlers can be an array, indicating we want to merge a bunch of
-    // handlers together.
-    let allHandlers;
-    if( Array.isArray( handlers ) ) {
-        checkOverlappingHandlers( handlers );
-        allHandlers = Object.assign.apply( {}, handlers );
-    }
-    else
-        allHandlers = handlers;
-
-    return function reducer( state = initialState, action ) {
-
-        // Check for a local action.
-        // ASSUMPTION: Actions may appear only once in any tree.
-        if( action && allHandlers.hasOwnProperty( action.type ) )
-            return allHandlers[action.type]( state, action );
-
-        // Check for subreducers.
-        else if( subreducers ) {
-            let subState = {};
-            Object.keys( subreducers ).forEach( key => {
-                subState[key] = subreducers[key]( state[key], action );
-            });
-            return Object.assign( {}, state, subState );
-        }
-
-        // Check for a default action.
-        else if( allHandlers.hasOwnProperty( 'DEFAULT' ) )
-            return allHandlers.DEFAULT( state, action );
-
-        else
-            return state;
-    }
+// Create an array reducer.
+export function createArrayReducer( handlers, initialState = [] ) {
+  return createReducer( handlers, initialState );
 }
 
 // Take an array of objects and return a mapping from a uniquely
 // identified field to the objects.
 export function toObjectMap( state, key = 'id' ) {
-    if( Array.isArray( state ) ) {
-        let byId = {};
-        state.forEach( item => byId[item[key]] = item );
-        return byId;
-    }
-    else
-        return state;
+  if( Array.isArray( state ) ) {
+    let byId = {};
+    state.forEach( item => byId[item[key]] = item );
+    return byId;
+  }
+  else
+    return state;
 }
 
 // Similar to `toObjectMap`, but instead of mapping to the objects,
 // map to the indices of the objects in the original array.
 export function toIndexMap( state, key = 'id' ) {
-    if( Array.isArray( state ) ) {
-        let byId = {};
-        state.forEach( ( item, ii ) => byId[item[key]] = ii );
-        return byId;
-    }
-    else
-        return state;
+  if( Array.isArray( state ) ) {
+    let byId = {};
+    state.forEach( ( item, ii ) => byId[item[key]] = ii );
+    return byId;
+  }
+  else
+    return state;
 }
 
 // A handler that applies `toObjectMap` on incoming arrays,
 export function objectListHandler( itemReducer, { actionKey = 'id', itemKey = 'id' }) {
-    return {
+  return {
 
-        DEFAULT( state = {}, action ) {
-            if( action &&
-                action.hasOwnProperty( actionKey ) &&
-                state.hasOwnProperty( action[actionKey] ) )
-            {
-                return {
-                    ...state,
-                    [ itemKey ]: itemReducer( state[action[actionKey]], action )
-                };
-            }
-            else
-                return toObjectList( state, itemKey );
-        }
+    DEFAULT( state = {}, action ) {
+      if( action &&
+        action.hasOwnProperty( actionKey ) &&
+        state.hasOwnProperty( action[actionKey] ) )
+      {
+        return {
+          ...state,
+          [ itemKey ]: itemReducer( state[action[actionKey]], action )
+        };
+      }
+      else
+        return toObjectList( state, itemKey );
     }
+  }
 }
 
 // A handler that extracts elements from arrays.
 export function arrayHandler( itemReducer, indexKey = 'index' ) {
-    return {
+  return {
 
-        DEFAULT( state = [], action ) {
-            if( action &&
-                action.hasOwnProperty( indexKey ) &&
-                (action[indexKey] >= 0 && action[indexKey] < state.length) )
-            {
-                let index = action[indexKey];
-                return [
-                    ...state.slice( 0, index ),
-                    itemReducer( state[index], action ),
-                    ...state.slice( index + 1 )
-                ]
-            }
-            else
-                return state;
-        }
+    DEFAULT( state = [], action ) {
+      if( action &&
+        action.hasOwnProperty( indexKey ) &&
+        (action[indexKey] >= 0 && action[indexKey] < state.length) )
+      {
+        let index = action[indexKey];
+        return [
+          ...state.slice( 0, index ),
+          itemReducer( state[index], action ),
+          ...state.slice( index + 1 )
+        ]
+      }
+      else
+        return state;
     }
+  };
+}
+
+// Manages an array of objects each of which has a unique identifier.
+// Tracks the objects in an object with an array and a mapping.
+export function modelArrayHandler( types, modelReducer, indexKey = 'index', idKey = 'id' ) {
+  const arrayReducer = createArrayReducer( arrayHandler( modelReducer, indexKey ) );
+  if( types.init === undefined )
+    throw 'Must supply at least an initialise type.';
+  let handler = {
+
+    [ types.init ]( state = [], action ) {
+      let obj = {
+        objects: action.results,
+        map: toIndexMap( action.results, idKey )
+      };
+      let get = function( id ) { return this.objects[this.map[id]]; }
+      get.bind( obj );
+      obj.get = get;
+      return obj;
+    },
+
+    DEFAULT( state = {}, action ) {
+      return {
+        ...state,
+        objects: arrayReducer( state, action )
+      };
+    }
+  };
+  if( types.update ) {
+
+    handler[types.update] = function( state = {}, action ) {
+      const { objects = [], map = {} } = state || {};
+
+      // Ensure we have an array of objects to add.
+      let results = action.results;
+      if( !Array.isArray( action.results ) )
+        results = [ action.results ];
+
+      // Create duplicates of the incoming state.
+      let newObjects = [ ...objects ], newMap = { ...map };
+
+      // Add each object.
+      results.forEach( newObj => {
+        const id = newObj.id;
+        if( id === undefined )
+          throw 'Cannot update a model array with no ID.';
+
+        let index = newMap[id];
+        if( index === undefined )
+          index = newObjects.length;
+
+        newObjects[index] = modelReducer( newObj, action );
+        newMap[id] = index;
+      });
+
+      return {
+        objects: newObjects,
+        map: newMap
+      };
+    }
+  }
+  return handler;
 }
 
 // A handler for async actions.
 export function asyncHandler( key, actionTypes, reducer ) {
-    let handlers = {
+  let handlers = {
 
-        [ actionTypes.REQUEST ]( state, action ) {
-            return {
-                ...state,
-                [ key + 'Loading' ]: true
-            };
-        },
+    [ actionTypes.REQUEST ]( state, action ) {
+      return {
+        ...state,
+        [ key + 'Loading' ]: true
+      };
+    },
 
-        [ actionTypes.SUCCESS ]( state, action ) {
-            return {
-                ...state,
-                [ key ]: (reducer ? reducer : x => x)( action.results, action ),
-                [ key + 'Error' ]: undefined,
-                [ key + 'Loading' ]: false
-            };
-        },
+    [ actionTypes.SUCCESS ]( state, action ) {
+      return {
+        ...state,
+        [ key ]: (reducer ? reducer : ( x, y ) => y.results)( state[key], action ),
+        [ key + 'Error' ]: undefined,
+        [ key + 'Loading' ]: false
+      };
+    },
 
-        [ actionTypes.FAILURE ]( state, action ) {
-            return {
-                ...state,
-                [ key + 'Error' ]: action.error,
-                [ key + 'Loading' ]: false
-            };
-        }
-    };
-    if( reducer ) {
-        handlers.SUBREDUCERS = {
-            [ key ]: reducer
-        }
+    [ actionTypes.FAILURE ]( state, action ) {
+      return {
+        ...state,
+        [ key ]: (reducer ? reducer : ( x, y ) => x)( state[key], action ),
+        [ key + 'Error' ]: action.error,
+        [ key + 'Loading' ]: false
+      };
     }
-    return handlers;
+  };
+  if( reducer ) {
+    handlers.SUBREDUCERS = {
+      [ key ]: reducer
+    }
+  }
+  return handlers;
 }
 
 // Do I even need this guy anymore?
 export function oneInListReducer( state, action ) {
-    let current = new Set( state );
-    let all = new Set( action.options );
-    let newList = [ ...current ].filter( x => all.has( x ) );
-    newList.push( action.selected );
-    return newList;
+  let current = new Set( state );
+  let all = new Set( action.options );
+  let newList = [ ...current ].filter( x => all.has( x ) );
+  newList.push( action.selected );
+  return newList;
 }
--- test/test.js
+++ test/test.js
@@ -1,57 +1,198 @@
 import assert from 'assert'
 import { asyncAction } from '../src/actions'
+import { createReducer, modelArrayHandler } from '../src/reducers'
 
 describe( 'actions', function() {
 
-    describe( 'asyncAction', function() {
+  describe( 'asyncAction', function() {
 
-        it( 'capitalises the name', function() {
-            let func = asyncAction( 'test_name', () => {} );
-            assert.deepEqual( func.TEST_NAME, {
-                REQUEST: 'TEST_NAME_REQUEST',
-                SUCCESS: 'TEST_NAME_SUCCESS',
-                FAILURE: 'TEST_NAME_FAILURE'
-            })
-        });
-
-        it( 'creates types', function() {
-            let func = asyncAction( 'TEST_NAME', () => {} );
-            assert.deepEqual( func.TEST_NAME, {
-                REQUEST: 'TEST_NAME_REQUEST',
-                SUCCESS: 'TEST_NAME_SUCCESS',
-                FAILURE: 'TEST_NAME_FAILURE'
-            })
-        });
-
-        it( 'creates action creator', function() {
-            let func = asyncAction( 'TEST_NAME', () => {} );
-            assert.equal( typeof func(), 'function' );
-        });
-
-        describe( 'created action', function() {
-
-            it( 'calls dispatch with request', function( done ) {
-                let actionCreator = asyncAction( 'test_name', () => {} );
-                actionCreator()( action => {
-                    assert.equal( action.type, actionCreator.TEST_NAME.REQUEST );
-                    done();
-                });
-            });
-
-            it( 'calls provided action', function( done ) {
-                let actionCreator = asyncAction( 'test_name', () => {
-                    assert( true );
-                    done();
-                });
-                actionCreator()( action => {} );
-            });
-
-            it( 'returns result of provided action', function() {
-                let actionCreator = asyncAction( 'test_name', () => {
-                    return 'hello';
-                });
-                assert.equal( actionCreator()( action => {} ), 'hello' );
-            });
-        });
+    it( 'capitalises the name', function() {
+      let func = asyncAction( 'test_name', () => {} );
+      assert.deepEqual( func.types, {
+        REQUEST: 'TEST_NAME_REQUEST',
+        SUCCESS: 'TEST_NAME_SUCCESS',
+        FAILURE: 'TEST_NAME_FAILURE'
+      })
     });
+
+    it( 'creates types', function() {
+      let func = asyncAction( 'TEST_NAME', () => {} );
+      assert.deepEqual( func.types, {
+        REQUEST: 'TEST_NAME_REQUEST',
+        SUCCESS: 'TEST_NAME_SUCCESS',
+        FAILURE: 'TEST_NAME_FAILURE'
+      })
+    });
+
+    it( 'creates action creator', function() {
+      let func = asyncAction( 'TEST_NAME', () => {} );
+      assert.equal( typeof func(), 'function' );
+    });
+
+    describe( 'created action', function() {
+
+      it( 'calls dispatch with request', function( done ) {
+        let actionCreator = asyncAction( 'test_name', () => {} );
+        actionCreator()( action => {
+          assert.equal( action.type, actionCreator.types.REQUEST );
+          done();
+        });
+      });
+
+      it( 'calls provided action', function( done ) {
+        let actionCreator = asyncAction( 'test_name', () => {
+          assert( true );
+          done();
+        });
+        actionCreator()( action => {} );
+      });
+
+      it( 'returns result of provided action', function() {
+        let actionCreator = asyncAction( 'test_name', () => {
+          return 'hello';
+        });
+        assert.equal( actionCreator()( action => {} ), 'hello' );
+      });
+    });
+  });
+});
+
+describe( 'reducers', function() {
+
+  describe( 'modelArrayHandler', function() {
+    const array = [
+      { id: 10, val: 100 },
+      { id: 20, val: 200 },
+      { id: 30, val: 300 }
+    ];
+    const reducer = createReducer( modelArrayHandler({ init: 'INIT_TYPE', update: 'UPDATE_TYPE' }, ( x, y ) => x ) );
+
+    it( 'stores array under `objects`', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      assert.deepEqual( state.objects, array );
+    });
+
+    it( 'creates mapping', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      assert.deepEqual( state.map, { 10: 0, 20: 1, 30: 2 } );
+    });
+
+    it( 'keeps mapping after passing to sub-reducer', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      state = reducer( state, { type: 'GOTO_SUB' } );
+      assert.deepEqual( state.map, { 10: 0, 20: 1, 30: 2 } );
+    });
+
+    it( 'gets objects', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      assert.deepEqual( state.get( 20 ), { id: 20, val: 200 } );
+      assert.deepEqual( state.get( 10 ), { id: 10, val: 100 } );
+    });
+
+    it( 'adds new object', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      state = reducer( state, { type: 'UPDATE_TYPE', results: { id: 40, val: 400 } });
+      assert.deepEqual( state, {
+        objects: [
+          { id: 10, val: 100 },
+          { id: 20, val: 200 },
+          { id: 30, val: 300 },
+          { id: 40, val: 400 }
+        ],
+        map: {
+          10: 0,
+          20: 1,
+          30: 2,
+          40: 3
+        }
+      });
+    });
+
+    it( 'adds array of new objects', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      state = reducer( state, { type: 'UPDATE_TYPE', results: [
+        { id: 50, val: 500 },
+        { id: 40, val: 400 },
+      ]});
+      assert.deepEqual( state, {
+        objects: [
+          { id: 10, val: 100 },
+          { id: 20, val: 200 },
+          { id: 30, val: 300 },
+          { id: 50, val: 500 },
+          { id: 40, val: 400 },
+        ],
+        map: {
+          10: 0,
+          20: 1,
+          30: 2,
+          50: 3,
+          40: 4,
+        }
+      });
+    });
+
+    it( 'updates exsting object', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      state = reducer( state, { type: 'UPDATE_TYPE', results: { id: 20, val: 600 } });
+      assert.deepEqual( state, {
+        objects: [
+          { id: 10, val: 100 },
+          { id: 20, val: 600 },
+          { id: 30, val: 300 },
+        ],
+        map: {
+          10: 0,
+          20: 1,
+          30: 2
+        }
+      });
+    });
+
+    it( 'updates array of exsting objects', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      state = reducer( state, { type: 'UPDATE_TYPE', results: [
+        { id: 20, val: 600 },
+        { id: 10, val: 700 },
+      ]});
+      assert.deepEqual( state, {
+        objects: [
+          { id: 10, val: 700 },
+          { id: 20, val: 600 },
+          { id: 30, val: 300 },
+        ],
+        map: {
+          10: 0,
+          20: 1,
+          30: 2
+        }
+      });
+    });
+
+    it( 'adds and updates objects', function() {
+      let state = reducer( [], { type: 'INIT_TYPE', results: array } );
+      state = reducer( state, { type: 'UPDATE_TYPE', results: [
+        { id: 50, val: 500 },
+        { id: 20, val: 600 },
+        { id: 10, val: 700 },
+        { id: 40, val: 400 },
+      ]});
+      assert.deepEqual( state, {
+        objects: [
+          { id: 10, val: 700 },
+          { id: 20, val: 600 },
+          { id: 30, val: 300 },
+          { id: 50, val: 500 },
+          { id: 40, val: 400 },
+        ],
+        map: {
+          10: 0,
+          20: 1,
+          30: 2,
+          40: 4,
+          50: 3,
+        }
+      });
+    });
+  });
 });
